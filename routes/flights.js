7364d8d15a33e0b273c110406605728bdf79cfa1
amirb <amir.bullock9@gmail.com>
2023-04-15 22:45:55 -0400

    stub up flights index

--- routes/flights.js
+++ routes/flights.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import * as flightsCtrl from '../controllers/flights.js'
+
 const router = Router()
 
-// GET localhost:3000/users
-  router.get('/', flightsCtrl.index)
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
 
 export { router }
