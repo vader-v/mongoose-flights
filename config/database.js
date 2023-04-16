3b1c8a295ca2a12e58d02b9f20d679db44e3bfec
amirb <amir.bullock9@gmail.com>
2023-04-15 20:29:41 -0400

    update database name to flights, add flights directory and flight model

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
 // shortcut to mongoose.connection object
-const db = mongoose.connection
+const flights = mongoose.connection
 
 mongoose.connect(process.env.DATABASE_URL)
 	
-db.on('connected', function() {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+flights.on('connected', function() {
+  console.log(`Connected to MongoDB ${flights.name} at ${flights.host}:${flights.port}`)
 })
