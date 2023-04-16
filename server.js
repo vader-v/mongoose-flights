392e126dbb0b3b5542fe4924d6f268b51b0594e1
amirb <amir.bullock9@gmail.com>
2023-04-15 20:07:16 -0400

    require database in server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
-
+import './config/database.js'
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/users.js'
