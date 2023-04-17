25062e99e1547eb56fa5c8481de89b02126df209
amirb <amir.bullock9@gmail.com>
2023-04-17 00:47:36 -0400

    install method override and import

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
 import './config/database.js'
+import methodOverride from 'method-override'
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/flights.js'
@@ -25,7 +26,7 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
-
+app.use(methodOverride('_method'))
 // mount imported routes
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
