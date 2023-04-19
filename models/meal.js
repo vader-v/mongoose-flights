6e3ce8f69fd85f010b8374fc07d92202f8dc93ba
amirb <amir.bullock9@gmail.com>
2023-04-18 23:50:17 -0400

    add meal schema

---
+++ models/meal.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const mealSchema = new Schema ({
+  name: {
+    type: String,
+    required: true,
+  }
+}, {
+  timestamps: true
+})
+
+const Meal = mongoose.model('Meal', mealSchema)
+
+export {
+  Meal
+}
